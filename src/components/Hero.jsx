8114aa08b0c580f0ded6c95751ba4ee562de7da9
saloni <saloni.bamane29@gmail.com>
2024-10-23 15:03:56 +0530

    Hamburger Changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,21 +3,25 @@ import arrowdown from "../assets/images/arrow_downward_24px.svg";
 function Hero() {
   return (
     <>
-      <div className="absolute top-[30%] z-10">
-        <div className="flex w-fit flex-col justify-center gap-8 px-16 md:px-32 lg:px-64 xl:px-[450px]">
+      <div className="absolute top-32 z-[5] mb-5 md:top-[15%] lg:top-[30%]">
+        <div className="flex w-fit flex-col justify-center gap-1 px-16 md:gap-8 md:px-32 lg:px-32 xl:px-44 2xl:px-[28rem]">
           <div className="font-gilroy font-extrabold uppercase text-accent">
-            <span className='before:absolute before:h-[2px] before:w-[72px] before:-translate-x-0 before:translate-y-3 before:bg-accent before:content-[""]'>
-              <p className="pl-24 uppercase tracking-[0.375rem]">
+            <span className='before:content-[""] md:before:absolute md:before:h-[2px] md:before:w-[72px] md:before:-translate-x-0 md:before:translate-y-3 md:before:bg-accent'>
+              <p className="uppercase tracking-[0.375rem] md:pl-24">
                 A Hiking guide
               </p>
             </span>
           </div>
-          <h1 className="font-chronicle text-[56px] font-semibold capitalize leading-normal md:text-[88px] md:leading-[100px]">
+          <h1 className="font-chronicle text-3xl font-semibold capitalize leading-normal sm:text-[56px] md:text-[80px] md:leading-[100px] xl:text-[88px]">
             Be prepared for the Mountains and beyond!
           </h1>
-          <div className="flex items-center gap-4">
-            <p className="font-gilroy text-lg">scroll down</p>
-            <img src={arrowdown} alt="scroll_arrow" />
+          <div className="flex h-5 items-center gap-4">
+            <p className="font-gilroy text-sm lg:text-lg">scroll down</p>
+            <img
+              src={arrowdown}
+              alt="scroll_arrow"
+              className="h-auto w-3 md:h-5 md:w-4"
+            />
           </div>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import account from "../assets/images/acc.svg";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { IoCloseOutline } from "react-icons/io5";
 function Navbar() {
   const navlinks = [
     {
@@ -17,10 +18,16 @@ function Navbar() {
     },
   ];
 
-  // const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+    document.body.style.overflow = isDrawerOpen ? "unset" : "hidden";
+  };
+
   return (
-    <>
-      <header className="absolute z-10 w-full px-20 pt-16">
+    <div>
+      <header className="absolute z-30 w-full px-20 pt-16">
         <nav className="flex items-center justify-between">
           <a href="/" className="font-chronicle text-3xl font-bold">
             MNTN
@@ -52,17 +59,18 @@ function Navbar() {
               </div>
             </a>
           </div>
-          <button
-            onClick={() => setIsDrawerOpen(!isDrawerOpen)}
-            className="md:hidden"
-          >
+          <button onClick={toggleDrawer} className="md:hidden">
             <GiHamburgerMenu />
           </button>
         </nav>
       </header>
-      {/* {isDrawerOpen && (
-        <div className="absolute left-0 top-0 h-screen w-screen bg-white text-black md:hidden">
-          <ul className="flex flex-col items-center bg-gray-100 p-4">
+      {isDrawerOpen && (
+        <div className="absolute right-0 top-0 z-10 h-full w-full translate-x-0 bg-black/50 px-20 pt-16 backdrop-blur-sm transition-all md:hidden md:-translate-x-full">
+          <ul className="absolute right-0 top-0 z-50 flex h-screen w-56 flex-col items-center gap-8 bg-gray-100 p-8 text-black">
+            <IoCloseOutline
+              onClick={toggleDrawer}
+              className="mb-8 mt-10 cursor-pointer text-3xl"
+            />
             {navlinks.map((nav) => {
               return (
                 <li key={nav.label}>
@@ -72,20 +80,20 @@ function Navbar() {
                 </li>
               );
             })}
+            <a href="#account">
+              <div className="flex w-fit items-center justify-end gap-2 rounded-md bg-backgroundColor p-4">
+                <span>
+                  <img src={account} alt="" />
+                </span>
+                <button className="font-gilroy text-[17px] font-medium text-white">
+                  Account
+                </button>
+              </div>
+            </a>
           </ul>
-          <a href="#account">
-            <div className="flex items-center gap-2">
-              <span>
-                <img src={account} alt="" />
-              </span>
-              <button className="font-gilroy text-[17px] font-medium">
-                Account
-              </button>
-            </div>
-          </a>
         </div>
-      )} */}
-    </>
+      )}
+    </div>
   );
 }
 
